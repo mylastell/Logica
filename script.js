0189157e73938b278e9409b600db7f9edcdd3ee6
unknown <mylastell>
2022-06-25 17:05:51 -0300

    finish

---
+++ script.js
@@ -0,0 +1,32 @@
+// Crie um objeto que receba ao menos três propriedades sobre você.
+let pessoa = [
+    {
+        nome: "Mylena",
+        idade: 22,
+        faculdade: "Uerj"
+    },
+]
+console.log(pessoa)
+// Adicione uma nova propriedade sem alterar seu objeto inicial.
+pessoa.time ="Botafogo"
+console.log(pessoa)
+// Remova uma propriedade desse objeto.
+delete pessoa.time
+console.log(pessoa)
+//Mostre no console todas as propriedades desse objeto.
+console.log(pessoa)
+// Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+let cadastro = [
+    {
+        nome: "Jésus",
+        idade: 23,
+        telefone: 980028922,
+        amigos: ("Stella, Andressa, Raquel, Waleska"),
+    }
+]
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+
+// Mostre no console o nome de um amigo de cada lista.
+
+console.log(cadastro)
