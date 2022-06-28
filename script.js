b877c6ef0ba68c51428cacedb353c01d4a39e59c
Myla <104770718+mylastell@users.noreply.github.com>
2022-06-28 15:40:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,26 +7,58 @@ let pessoa = [
     },
 ]
 console.log(pessoa)
+
 // Adicione uma nova propriedade sem alterar seu objeto inicial.
 pessoa.time ="Botafogo"
 console.log(pessoa)
+
 // Remova uma propriedade desse objeto.
 delete pessoa.time
 console.log(pessoa)
+
 //Mostre no console todas as propriedades desse objeto.
 console.log(pessoa)
+
 // Crie um array  chamado "cadastro" contendo ao menos 5 objetos. 
+//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
+//Na propriedade amigos, adicione ao menos 4 itens.
+
 let cadastro = [
     {
         nome: "Jésus",
         idade: 23,
         telefone: 980028922,
-        amigos: ("Stella, Andressa, Raquel, Waleska"),
-    }
+        amigos: ["Stella", "Andressa", "Raquel", "Waleska", "Helder"],
+    },
+     {
+          nome: "Andressa",
+          idade: 19,
+          telefone: 222131,
+          amigos: ["Stella", "Jésus", "Raquel", "Waleska", "Helder"]
+   }, 
+   {
+           nome: "Raquel",
+           idade: 22,
+           telefone: 3123123,
+           amigos: ["Stella", "Andressa", "Jésus", "Waleska", "Helder"]
+     }, 
+     {
+           nome: "Waleska",
+           idade: 18,
+           telefone: 4342342,
+           amigos: ["Stella", "Andressa", "Raquel", "Jésus", "Helder"]
+     }, 
+     {
+           nome: "Helder",
+           idade: 20,
+           telefone: 3123123,
+           amigos: ["Stella", "Andressa", "Raquel", "Waleska", "Jésus"]
+     
+     }
 ]
-//Cada objeto deve receber as seguintes propriedades: nome,  idade,  telefone, amigos. 
-//Na propriedade amigos, adicione ao menos 4 itens.
 
 // Mostre no console o nome de um amigo de cada lista.
 
-console.log(cadastro)
+for(let contador = 0; contador < cadastro.length; contador++ ){
+    console.log(cadastro[contador].amigos[contador])
+}
